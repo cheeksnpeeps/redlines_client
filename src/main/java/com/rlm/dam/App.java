f99fe91a020e6d7a7c83d0af0c583eed62159577
Don Livanec <donlivanec@gmail.com>
2012-02-28 09:38:12 -0500

    Add springsource repos

---
+++ src/main/java/com/rlm/dam/App.java
@@ -0,0 +1,13 @@
+package com.rlm.dam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
